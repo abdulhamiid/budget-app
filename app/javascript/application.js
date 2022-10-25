65caa3666a150cb8e914520822c87181ea318ffe
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-25 15:11:05 +0100

    Add menu toggler

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.getElementById("menu").addEventListener("click", function() {
+  document.getElementById("toggle-bar").classList.toggle("hidden");
+  document.getElementById("mcv").classList.add("blur");
+});
+
+document.getElementById("bi-cls").addEventListener("click", function() {
+  document.getElementById("toggle-bar").classList.toggle("hidden");
+  document.getElementById("mcv").classList.remove("blur");
+});
