3e765346b11e8b0c57b91a922c3ff6a07214c84f
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-25 15:03:25 +0100

    Arrange transanction based on latest

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :user
 
-  has_many :entities
+  has_many :entities, dependent: :destroy
+
+  def recent_transactions
+    entities.order('created_at Desc')
+  end
 end
