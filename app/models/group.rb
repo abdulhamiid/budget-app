67499f056f1be00560bfc62ecd95e3bfa363572e
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-26 04:23:42 +0100

    Add image icon to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
 
   has_many :entities, dependent: :destroy
+  has_one_attached :icon
 
   def recent_transactions
     entities.order('created_at Desc')
