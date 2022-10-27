6b1ca325d4a4c9b2a42587557bd3c813fd1b9d68
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-27 10:36:37 +0100

    Fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     groups_path
   end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  def index
-  end
+  def index; end
 end
